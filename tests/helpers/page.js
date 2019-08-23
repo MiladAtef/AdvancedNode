bcc8e3daddf917336320d4dcca25f03c5948c68c
Milad <meladattef2017@gmail.com>
2019-08-23 04:15:28 +0200

    just for travis

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -29,7 +29,7 @@ class CustomPage {
 		await this.page.setCookie({ name: 'session', value: session });
 		await this.page.setCookie({ name: 'session.sig', value: sig });
 		await this.page.goto('http://localhost:3000/blogs'); // to refresh the page
-		await this.page.waitFor("a[href='/auth/logout']"); // to wait until the button is rendered on the screen
+		await this.page.waitFor("a[href='/auth/logout']"); // to wait until the button is rendered on the screeeeen
 	}
 
 	// an easier implementation of '$eval' function
